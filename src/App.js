e44ec925b034eaecea8a3841e61ff74985f9e405
Angelo Galope <105281585+angelogalope@users.noreply.github.com>
2023-07-02 13:50:25 +0800

    Media Query

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import { Link } from "react-scroll";
+import Navbar from "./components/Navbar";
 
-const Navigation = () => {
-  return (
-    <div className="w-[1324.02px] h-[73.48px] left-[297.99px] top-[23px] absolute">
-        <button className="left-[906.01px] top-[15px] absolute text-zinc-950 text-[20px] font-semibold hover:text-slate-400">
-          <Link to="stack" spy={true} smooth={true} offset={0} duration={500}>Stack</Link>
-        </button>
-        <button className="left-[779.01px] top-[15px] absolute text-zinc-950 text-[20px] font-semibold hover:text-slate-400">
-          <Link to="about" spy={true} smooth={true} offset={0} duration={500}>About</Link>
-        </button>
-        <button className="left-[1030.01px] top-[15px] absolute text-zinc-950 text-[20px] font-semibold hover:text-slate-400">
-          <Link to="project" spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
-        </button>
-        <button className="left-[1177.01px] top-[15px] absolute text-zinc-950 text-[20px] font-semibold hover:text-slate-400">
-          <Link to="contact" spy={true} smooth={true} offset={0} duration={500}>Contact</Link>
-        </button>
-        <img
-          alt="Logo"
-          className="w-[54px] h-[54px] left-[69.01px] top-0 absolute"
-          src="Logo.png"
-        />
-        <div className="w-[1324.02px] h-[0px] left-0 top-[73.48px] absolute border border-black"></div>
-      </div>
-  );
-};
 
 const HeroSection = () => {
   return (
@@ -65,7 +42,7 @@ function App () {
     <div>
       <div id="home" className="w-[1903px] h-[2706px] bg-white">
       {/* Navigation Bar */}
-      <Navigation />
+      <Navbar />
 
       {/* Hero Page */}
       <HeroSection/>
